dedf21344bc3ea9e6171f9f1a4b2a2133eb95639
Ryusei081 <2201125@s.asojuku.ac.jp>
2024-01-10 12:46:00 +0900

    あ

---
+++ src/db-connect.php
@@ -0,0 +1,8 @@
+<?php
+    const SERVER = 'mysql214.phy.lolipop.lan';
+    const DBNAME = 'LAA1517447-final';
+    const USER = 'LAA1517447';
+    const PASS = 'Pass0818';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
---
+++ src/menue.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>メニュー</title>
+</head>
+<body>
+    <h1>本管理メニュー</h1>
+    <table>
+    <form action="select.php" method="post">
+        <input type="submit" value="一覧">
+    </form>
+    <form action="insert.php" method="post">
+        <input type="submit" value="登録">
+    </form>
+    <form action="update.php" method="post">
+        <input type="submit" value="更新">
+    </form>
+    <form action="delete.php" method="post">
+        <input type="submit" value="削除">
+    </form>
+    </table>
+</body>
+</html>
